41bf2731293b2a58b49dafc9da4688679fdfe214
donald.taylor02 <donald.taylor02@leonardocompany.com>
2019-09-08 12:33:02 +0100

    refactor Working on simulinkToGraph2 method

--- LeonardoDigitalThread/src/LeonardoDigitalThread.java
+++ LeonardoDigitalThread/src/LeonardoDigitalThread.java
@@ -47,7 +47,7 @@ import org.leonardo.swt.swing_compat.EmbeddedSwingComposite;
 
 public class LeonardoDigitalThread {
 	private Graph graph;
-	
+
 	static String readFileWithDefault(String path, String defaultedValue) {
 		try {
 			return readFile(path, Charset.defaultCharset());
@@ -56,24 +56,24 @@ public class LeonardoDigitalThread {
 			return defaultedValue;
 		}
 	}
-	
+
 	static String readFile(String path, Charset encoding) throws IOException {
 		byte[] encoded = Files.readAllBytes(new File(path).toPath());
 		return new String(encoded, encoding);
 	}
-	
+
 	static final String CSS = readFileWithDefault(LeonardoDigitalThread.class.getResource("style.css").getFile(), "");
-	
+
 	class GraphView{
 		Viewer viewer;
 		ViewPanel vP_new;
-		
+
 		public GraphView(Viewer viewer, ViewPanel vP_new) {
 			this.viewer = viewer;
 			this.vP_new = vP_new;
 		}
 	}
-	
+
 	static ViewPanel createGraphView(Graph graph, boolean autoLayout) {
 		Viewer viewer = new Viewer(graph, Viewer.ThreadingModel.GRAPH_IN_GUI_THREAD);
 		if(autoLayout) {
@@ -84,25 +84,17 @@ public class LeonardoDigitalThread {
 		viewer.setCloseFramePolicy(CloseFramePolicy.CLOSE_VIEWER);
 		return vP_new;
 	}
-	
-	public static void main(String[] args) {
-		Display display = new Display();
-		Shell shell = new Shell(display);
-		shell.setLayout(new GridLayout(1, false));
 
-		final MultiGraph graphNew = new MultiGraph("Graph");
-		graphNew.addAttribute("ui.quality");
-		graphNew.addAttribute("ui.antialias");
-		graphNew.addAttribute("ui.stylesheet", CSS);
-		
+	public static void main(String[] args) {
+
 		SimulinkModel model = new SimulinkModel();
 		model.setName("SimulinkModel");
 		String libraryPath = "C:\\Program Files\\MATLAB\\R2019a\\bin\\win64";
 		String engineJarPath = "C:\\Program Files\\MATLAB\\R2019a\\extern\\engines\\java\\jar\\engine.jar";
 		String workingDir = "D:\\\\simscapeExperiment1";
 		//String modelfile = "D:\\simscapeExperiment1\\simple.slx";
-		//String modelfile = "D:\\simscapeExperiment1\\tank_circuit.slx";
-		String modelfile = "D:\\\\simscapeExperiment1\\ee_radio_am_receiver.slx";
+		String modelfile = "D:\\simscapeExperiment1\\tank_circuit.slx";
+		//String modelfile = "D:\\\\simscapeExperiment1\\ee_radio_am_receiver.slx";
 		boolean followLinks = true;
 		boolean openMatlabEditor = false;
 
@@ -117,76 +109,112 @@ public class LeonardoDigitalThread {
 		} catch (EolModelLoadingException e) {
 			e.printStackTrace();
 		}
+		final MultiGraph graphNew = new MultiGraph("Graph");
+		graphNew.addAttribute("ui.quality");
+		graphNew.addAttribute("ui.antialias");
+		graphNew.addAttribute("ui.stylesheet", CSS);
+		
+		//simulinkToGraph1(model, graphNew);
+		simulinkToGraph2(model, graphNew);
+
+		System.out.println("Finished Simulink Work!");
+
+		Display display = new Display();
+		Shell shell = new Shell(display);
+		shell.setLayout(new GridLayout(1, false));
+		EmbeddedSwingComposite embeddedComposite = new EmbeddedSwingComposite(shell, SWT.NONE) {
+			protected JComponent createSwingComponent() {
+				JScrollPane scrollPane = new JScrollPane();
+				ViewPanel viewPanel = createGraphView(graphNew, true);
+				scrollPane.setViewportView(viewPanel);
+				return scrollPane;
+			}
+		}; 
+		embeddedComposite.populate();
+		embeddedComposite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+		shell.open();
+		// run the event loop as long as the window is open
+		while (!shell.isDisposed()) {
+			// read the next OS event queue and transfer it to a SWT event
+			if (!display.readAndDispatch()) {
+				// if there are currently no other OS event to process sleep until the next OS event is available
+				display.sleep();
+			}
+		}
+		display.close();
+		System.exit(0);
+	}
+
+	private static void simulinkToGraph1(final SimulinkModel model, final MultiGraph graphNew) {
 		Map<String, Node> mapNodes = new HashMap<String, Node>();
 		Map<String, Edge> mapEdges = new HashMap<String, Edge>();
 		for(ISimulinkModelElement x : model.allContents()) {
 			try {
-			try {
-			Node n1 = mapNodes.get(x.getPath());
-			try {
-				Object parent = x.getProperty("Parent");
+				try {
+					Node n1 = mapNodes.get(x.getPath());
+					try {
+						Object parent = x.getProperty("Parent");
 
-				Object parameterList = x.getProperty("ObjectParameters");
+						Object parameterList = x.getProperty("ObjectParameters");
 
-				System.out.println("Skip " + parameterList.getClass());
-				
-				org.eclipse.epsilon.emc.simulink.types.Struct list = ( org.eclipse.epsilon.emc.simulink.types.Struct) parameterList;
-				Set<Entry<String, Object>> set = (Set<Entry<String, Object>>)list.entrySet();
-				for(Entry<String, Object> listEle : set) {
-					System.out.println("\t\t" + listEle.getKey() + " :: ");
-					for(Entry<String, Object> listValEle : ((com.mathworks.matlab.types.Struct)listEle.getValue()).entrySet()) {
-						System.out.println("\t\t\t" + listValEle.getKey() + " :: " + listValEle.getValue());
-						if(listValEle.getValue() instanceof String[]) {
-							for(String strVal : (String[])listValEle.getValue()) {
-								System.out.println("\t\t\t\t" + strVal);
+						System.out.println("Skip " + parameterList.getClass());
+
+						org.eclipse.epsilon.emc.simulink.types.Struct list = ( org.eclipse.epsilon.emc.simulink.types.Struct) parameterList;
+						Set<Entry<String, Object>> set = (Set<Entry<String, Object>>)list.entrySet();
+						for(Entry<String, Object> listEle : set) {
+							System.out.println("\t\t" + listEle.getKey() + " :: ");
+							for(Entry<String, Object> listValEle : ((com.mathworks.matlab.types.Struct)listEle.getValue()).entrySet()) {
+								System.out.println("\t\t\t" + listValEle.getKey() + " :: " + listValEle.getValue());
+								if(listValEle.getValue() instanceof String[]) {
+									for(String strVal : (String[])listValEle.getValue()) {
+										System.out.println("\t\t\t\t" + strVal);
+									}
+								}
 							}
+							System.out.println("\t\t Value: " + x.getProperty(listEle.getKey()));	
+
 						}
+
+						//Object parentObj = model.getElementById((String) parent);
+						//System.out.println(parent + " " + parentObj);
+					} catch (EolIllegalPropertyException e1) {
+						e1.printStackTrace();
 					}
-					System.out.println("\t\t Value: " + x.getProperty(listEle.getKey()));	
-		
+					if(n1 == null) {
+						n1 = graphNew.addNode(x.getPath());
+						n1.addAttribute("ui.label", x.getPath());
+						mapNodes.put(x.getPath(), n1);
+					}
+					Node typeNode = mapNodes.get(x.getType());
+					if(typeNode == null) {
+						typeNode = graphNew.addNode(x.getType());
+						typeNode.addAttribute("ui.label", x.getType());
+						mapNodes.put(x.getType(), typeNode);
+					}
+
+					Edge e = mapEdges.get(x.getPath()+"_"+x.getType());
+					if(e == null) {
+						e = graphNew.addEdge(x.getPath()+"_"+x.getType(), n1, typeNode);
+						mapEdges.put(x.getPath()+"_"+x.getType(), e);
+					}
+					System.out.println(x.hashCode());
+					System.out.println(x.getPath());
+				}catch(java.lang.NullPointerException nPE) {
+					System.err.println("Got null but continue " + x.getType());
 				}
-				
-				//Object parentObj = model.getElementById((String) parent);
-				//System.out.println(parent + " " + parentObj);
-			} catch (EolIllegalPropertyException e1) {
-				e1.printStackTrace();
-			}
-			if(n1 == null) {
-				n1 = graphNew.addNode(x.getPath());
-			 	n1.addAttribute("ui.label", x.getPath());
-				mapNodes.put(x.getPath(), n1);
-			}
-			Node typeNode = mapNodes.get(x.getType());
-			if(typeNode == null) {
-				typeNode = graphNew.addNode(x.getType());
-				typeNode.addAttribute("ui.label", x.getType());
-				mapNodes.put(x.getType(), typeNode);
-			}
 
-			Edge e = mapEdges.get(x.getPath()+"_"+x.getType());
-			if(e == null) {
-				e = graphNew.addEdge(x.getPath()+"_"+x.getType(), n1, typeNode);
-				mapEdges.put(x.getPath()+"_"+x.getType(), e);
-			}
-			System.out.println(x.hashCode());
-			System.out.println(x.getPath());
 			}catch(java.lang.NullPointerException nPE) {
-				System.err.println("Got null but continue " + x.getType());
+				System.err.println("Got null again but uuuuh keep going...");
 			}
-
-		}catch(java.lang.NullPointerException nPE) {
-			System.err.println("Got null again but uuuuh keep going...");
 		}
-		}
-        /*try {
+		/*try {
 			for(ISimulinkModelElement x : model.getAllOfType("SubSystem")) {
 				System.out.println(x);
 			}
 		} catch (EolModelElementTypeNotFoundException e) {
 			e.printStackTrace();
 		}*/
-        System.out.println("Finished Simulink Work!");
-		
+
 		/*Node nH1 = graphNew.addNode("Component Hello 1");
 		Node nH2 = graphNew.addNode("Component Hello 2");
 		Node nH3 = graphNew.addNode("Component Hello 3");
@@ -198,27 +226,68 @@ public class LeonardoDigitalThread {
 		graphNew.addEdge("e3a", nH3, nH3a);
 		graphNew.addEdge("e3b1", nH3, nH3b);
 		graphNew.addEdge("e3b2", nH3b, n1);*/
+	}
+	private static void simulinkToGraph2(final SimulinkModel model, final MultiGraph graphNew) {
+		Map<String, Node> mapNodes = new HashMap<String, Node>();
+		Map<String, Edge> mapEdges = new HashMap<String, Edge>();
+		for(ISimulinkModelElement x : model.allContents()) {
+			try {
+				try {
+					try {
+						Object parent = x.getProperty("Parent");
 
-		EmbeddedSwingComposite embeddedComposite = new EmbeddedSwingComposite(shell, SWT.NONE) {
-            protected JComponent createSwingComponent() {
-                JScrollPane scrollPane = new JScrollPane();
-                ViewPanel viewPanel = createGraphView(graphNew, true);
-                scrollPane.setViewportView(viewPanel);
-                return scrollPane;
-            }
-        }; 
-        embeddedComposite.populate();
-        embeddedComposite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
-        shell.open();
-		// run the event loop as long as the window is open
-		while (!shell.isDisposed()) {
-			// read the next OS event queue and transfer it to a SWT event
-			if (!display.readAndDispatch()) {
-				// if there are currently no other OS event to process sleep until the next OS event is available
-				display.sleep();
+						Object parameterList = x.getProperty("ObjectParameters");
+
+						System.out.println("Skip " + parameterList.getClass());
+
+						org.eclipse.epsilon.emc.simulink.types.Struct list = ( org.eclipse.epsilon.emc.simulink.types.Struct) parameterList;
+						Set<Entry<String, Object>> set = (Set<Entry<String, Object>>)list.entrySet();
+						for(Entry<String, Object> listEle : set) {
+							System.out.println("\t\t" + listEle.getKey() + " :: ");
+							for(Entry<String, Object> listValEle : ((com.mathworks.matlab.types.Struct)listEle.getValue()).entrySet()) {
+								System.out.println("\t\t\t" + listValEle.getKey() + " :: " + listValEle.getValue());
+								if(listValEle.getValue() instanceof String[]) {
+									for(String strVal : (String[])listValEle.getValue()) {
+										System.out.println("\t\t\t\t" + strVal);
+									}
+								}
+							}
+							System.out.println("\t\t Value: " + x.getProperty(listEle.getKey()));	
+
+						}
+
+						//Object parentObj = model.getElementById((String) parent);
+						//System.out.println(parent + " " + parentObj);
+					} catch (EolIllegalPropertyException e1) {
+						e1.printStackTrace();
+					}
+					Node n1 = mapNodes.get(x.getPath());
+					if(n1 == null) {
+						n1 = graphNew.addNode(x.getPath());
+						n1.addAttribute("ui.label", x.getPath());
+						mapNodes.put(x.getPath(), n1);
+					}
+					Node typeNode = mapNodes.get(x.getType());
+					if(typeNode == null) {
+						typeNode = graphNew.addNode(x.getType());
+						typeNode.addAttribute("ui.label", x.getType());
+						mapNodes.put(x.getType(), typeNode);
+					}
+
+					Edge e = mapEdges.get(x.getPath()+"_"+x.getType());
+					if(e == null) {
+						e = graphNew.addEdge(x.getPath()+"_"+x.getType(), n1, typeNode);
+						mapEdges.put(x.getPath()+"_"+x.getType(), e);
+					}
+					System.out.println(x.hashCode());
+					System.out.println(x.getPath());
+				}catch(java.lang.NullPointerException nPE) {
+					System.err.println("Got null but continue " + x.getType());
+				}
+
+			}catch(java.lang.NullPointerException nPE) {
+				System.err.println("Got null again but uuuuh keep going...");
 			}
 		}
-		display.close();
-		System.exit(0);
 	}
 }
